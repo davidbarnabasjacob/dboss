7c73b54b6809f36a1a752a2bb26359c421b9660f
David Barnabas Jacob <david.barnaboss@gmail.com>
2019-09-03 19:37:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log("Test");
+ var notification = new Notification('Notification title', {
+   icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+   body: 'Hey there! You\'ve been notified!',
+  });
+  notification.onclick = function() {
+   window.open('http://stackoverflow.com/a/13328397/1269037');
+  };
